23495ec2b0ac66375385ef5ec01587a7b63c43d1
Emerita Noree <emeritan@ox-t6.msk.21-school.ru>
2023-11-11 17:00:55 +0300

    Declarations

---
+++ src/expression_process.h
@@ -0,0 +1,9 @@
+#include "structs.h"
+
+char* get_lexem(char* str, struct expr_item* answer, int has_left_operand);
+
+struct vector expression_to_postfix(char* str); //если введенное выражение некорректно, вернет невалидный вектор
+// Есть функция vector_is_valid, которая это и проверяет
+// Также может быть невалидным если закончилась память, но разницы нет, мы работать не можем, выводи просто, что произошла ошибка
+
+double calculate_expression(struct vector expression, double x);
