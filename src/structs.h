d813411e048b27813817ce88ad79fd46593564ac
Emerita Noree <emeritan@ox-t6.msk.21-school.ru>
2023-11-11 15:58:19 +0300

    Data types

---
+++ src/structs.h
@@ -0,0 +1,24 @@
+enum oper_type
+{
+    PLUS, MINUS, MULTIPLY, DIVIDE, OPEN_B, CLOSE_B, SIN, COS, TAN, CTG, SQRT, LN, X, UNDEF
+};
+
+struct stack
+{
+    struct expr_item data;
+    struct stack* down;
+};
+
+struct vector
+{
+    struct expr_item* data;
+    int size;
+    int capacity;
+};
+
+struct expr_item
+{
+    enum oper_type type;
+    double value; // на случай если это константа
+    int (*calculate)(struct expr_item, struct stack*, double x); //указатель на функцию которая будет все вычислять
+};
